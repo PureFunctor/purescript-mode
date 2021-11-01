fd08b86b81c8e2a10694f2d2fd1ac2a33c45eab5
PureFunctor <purefunctor@gmail.com>
2021-11-01 21:30:39 +0800

    Treat ado as a reserved identifier

--- purescript-font-lock.el
+++ purescript-font-lock.el
@@ -178,7 +178,7 @@ Returns keywords suitable for `font-lock-keywords'."
           ;; spec syntax, but they are not reserved.
           ;; `_' can go in here since it has temporary word syntax.
           (regexp-opt
-           '("case" "class" "data" "default" "deriving" "do"
+           '("case" "class" "data" "default" "deriving" "ado" "do"
              "else" "if" "import" "in" "infix" "infixl"
              "infixr" "instance" "let" "module" "newtype" "of"
              "then" "type" "where" "_") 'words))
